ddb31f194c60fd03b572f10048cc20990a33c0f4
maarcmaarti <marcxeresa@gmail.com>
2025-11-10 15:41:32 +0100

    List.h terminado

--- List.h
+++ List.h
@@ -1,18 +1,29 @@
 #ifndef LIST_H
 #define LIST_H
 
-template <typename T> 
+#include <stdexcept>
+
+template <typename T>
 class List {
-    public:
-       void insert(int pos,T e){};
-       void append(T e){};
-       void prepend(T e){};
-       T remove(int pos){};
-       T get(int pos){};
-       int search(T e){};
-       bool empty(){};
-       int size();
-       // ... aquí los métodos virtuales puros
+public:
+
+    virtual ~List() {}
+
+    virtual void insert(int pos, T e) = 0;
+
+    virtual void append(T e) = 0;
+
+    virtual void prepend(T e) = 0;
+
+    virtual T remove(int pos) = 0;
+
+    virtual T get(int pos) = 0;
+
+    virtual int search(T e) = 0;
+
+    virtual bool empty() = 0;
+
+    virtual int size() = 0;
 };
 
 #endif
