780afcdc1f95941f6d5b51d5650c72267ad5a2c2
maarcmaarti <mmaroch1@upv.edu.es>
2025-11-10 22:03:25 +0100

    Node.h terminado

---
+++ Node.h
@@ -0,0 +1,24 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <ostream>
+using namespace std;
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node<T>* next;
+
+    Node(T data, Node<T>* next = nullptr) {
+        this->data = data;
+        this->next = next;
+    }
+
+    friend ostream& operator<<(ostream &out, const Node<T> &node) {
+        out << node.data;  // CORREGIDO: era node->data
+        return out;
+    }
+};
+
+#endif
